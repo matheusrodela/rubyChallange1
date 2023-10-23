c7c7ade4fceac0d9155e12a32fb57c16195d7956
Matheus Rodela <46487106+matheusrodela@users.noreply.github.com>
2023-10-23 20:49:30 -0300

    aula 4

---
+++ aula4.rb
@@ -0,0 +1,16 @@
+#1) Saida de dados na tela - print
+# Usuário digite seu nome
+print "Digite seu nome: "
+
+
+#2) Programa permite que o user preencha o nome
+#gets.chomp
+nome = gets.chomp
+
+print "Digite seu Sobrenome: "
+sobrenome = gets.chomp 
+
+
+#3) saida final
+#para chamar um dado #{var}
+puts "Oi, #{nome} #{sobrenome}!"
---
+++ aula4soma.rb
@@ -0,0 +1,12 @@
+#Usuário digita 1 número
+print "Digite um número inteiro: "
+num1 = gets.chomp.to_i
+
+#User digita outro num
+print "Digite outro número inteiro: "
+num2 = gets.chomp.to_i
+
+#soma
+soma = num1 + num2
+
+puts "A soma dos números é: #{soma}"
