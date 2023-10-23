1cba1722795891fb124412a883494600e6b775eb
Matheus Rodela <46487106+matheusrodela@users.noreply.github.com>
2023-10-23 20:56:30 -0300

    Desafio

---
+++ desafio.rb
@@ -0,0 +1,15 @@
+
+puts "Preciso que você forneça alguns dados para começar!"
+puts "Primeiro, digite seu nome:"
+
+nome = gets.chomp
+
+print "Agora, digite seu sobrenome:"
+
+sobrenome = gets.chomp
+
+print "Por fim, digite sua idade:"
+
+idade = gets.chomp
+
+puts "Nome: #{nome} #{sobrenome}; Idade: #{idade}"
