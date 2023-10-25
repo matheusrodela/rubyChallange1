b1a6f2dce8af4bd9b2e1d1d8d267fe09f9fa3ab6
Matheus Rodela <46487106+matheusrodela@users.noreply.github.com>
2023-10-24 21:34:40 -0300

    hashes and array

---
+++ AvancandoRuby/Colecoes/array.rb
@@ -0,0 +1,27 @@
+#Coleção Array
+
+#Criar array vazio -> nome = []
+
+#Add item | itens -> nome.push('O que vamos colocar') | nome.push('O que vamos colocar', 'outra coisa')
+
+#Organizando novos dados da lista -> nome.insert(0, 'nome1', 'nome2')
+
+#Acessando os elementos do array nome[numero] | nome[1..4] (acessar posições de 1 a 4)
+
+#Recuperar o primeiro item -> nome.first | nome.last (recuperar último)
+
+#Quantidade de elementos na array -> nome.count | nome.length
+
+#Checar se está vazio -> nome.empty?
+
+#Verificar se existe o item -> nome.include?('nome')
+
+#Excluir elementos
+    #elemento específico -> nome.delete_at(numero)
+
+    #excluir ultimo elemento -> nome.pop
+
+    #excluir primeiro elemento -> nome.shift
+
+    #excluir todos -> nome.clear
+
---
+++ AvancandoRuby/Colecoes/hashes.rb
@@ -0,0 +1,26 @@
+#Manipulando hashes
+#tembém é um tipo de lista, mas ela tem uma CHAVE: VALOR    
+
+#Criando um hash vazio = Hash.new
+    # animais = Hash.new
+
+#Podemos iniciar com pares CHAVE-VALOR variavel = {chave: 'valor', chave: 'valor'}
+    # animais = {ave: 'Tucano', peixe: 'Baiacu'}
+
+#adicione um nome item ao hash variavel [:nome inclusão] = "valor a ser add" -> se usar com chave que já existe ele vai substituir
+    # animais[:inseto] = "mosquito"
+
+#para retornar todas as chaves de um hash nome.keys
+    # animais.keys -> [:ave, :peixe, :inseto]
+
+#para retornar todos os valores de um hash nome.values
+    # animais.values -> ["Tucano", "Baiacu", "mosquito"]
+
+#para excluir um elemento nome.delete(:nome a ser exluido)
+    # animais.delete(:ave) -> {:peixe=>"Baiacu", :inseto=>"mosquito"}
+
+#descubra o tamanho do hash nome.size
+    # animais.size -> 2
+
+#verifique se o hash está vazio nome.empty?
+    #animais.empty? -> false
